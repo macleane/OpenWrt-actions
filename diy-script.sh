720629a9f83449c09bacef00e457ad19deffe4b8
macleane <macleane@163.com>
2024-03-26 13:53:44 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -85,6 +85,8 @@ echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.defa
 # git clone https://github.com/apollo-ng/luci-theme-darkmatter
 # git clone https://gitlab.stusta.de/stustanet/luci-theme-ssn
 # git clone https://github.com/LeoHao/PPIP_LUCI_THEME
+git clone --depth=1 -b js https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
+git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
 
 # 更改 Argon 主题背景
 #cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
